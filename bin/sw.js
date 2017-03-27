d4098b8803c99b0f476124e496c3d3d74ceffafc
clearbug <craftsman_gao@qq.com>
2017-03-27 16:54:52 +0800

    使用响应头中的 content-type 字段来决定是否以 JSON 格式解析响应体

--- bin/sw.js
+++ bin/sw.js
@@ -71,74 +71,59 @@ if (cbUtil.checkCookieValid(cookiesObj)) {
         })
         .then(res => {
             console.log(`\r\n单词 [${content}] 已成功添加到单词学习库。`);
-            process.exit();
+        })
+        .catch(error => {
+            console.log(error);
+        });
+} else {
+    var loginReqOptions = shanbayApi.loginReqOptions;
+    var loginInfo = {
+        username: config.username,
+        password: config.password
+    };
+    var loginPutData = JSON.stringify(loginInfo);
+    loginReqOptions.headers['Content-Length'] = loginPutData.length;
+    cbUtil.request(loginReqOptions, loginPutData)
+        .then(res => {
+            var cookiesObj = cbUtil.parseSetCookie(res.headers['set-cookie']);
+            fs.writeFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json'), JSON.stringify(cookiesObj));
+            var homepageReqOptions = shanbayApi.homepageReqOptions;
+            homepageReqOptions.headers.Cookie += cbUtil.getCookieStr(cookiesObj);
+            return cbUtil.request(homepageReqOptions);
+        })
+        .then(res => {
+            var cookiesObj = JSON.parse(fs.readFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json')));
+            var cookiesObj2 = cbUtil.parseSetCookie(res.headers['set-cookie']);
+            for (key in cookiesObj2) {
+                cookiesObj[key] = cookiesObj2[key];
+            }
+            fs.writeFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json'), JSON.stringify(cookiesObj));
+            var searchReqOptions = shanbayApi.searchReqOptions;
+            searchReqOptions.path += content;
+            return cbUtil.request(searchReqOptions);
+        })
+        .then(res => {
+            var data = res.body.data;
+            var cnDefinitions = data.definitions.cn;
+            console.log(`单词 [${content}] 含义如下：\r\n`);
+            for (var i = 0; i < cnDefinitions.length; i++) {
+                console.log(`[${i + 1}] ${cnDefinitions[i].pos} ${cnDefinitions[i].defn}`);
+            }
+
+            var learnReqOptions = shanbayApi.learnReqOptions;
+            var learnReqData = JSON.stringify({
+                id: data.id,
+                content_type: 'vocabulary'
+            });
+            learnReqOptions.headers['Content-Length'] = learnReqData.length;
+            var cookiesObj = JSON.parse(fs.readFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json')));
+            learnReqOptions.headers.Cookie += cbUtil.getCookieStr(cookiesObj);
+            return cbUtil.request(learnReqOptions, learnReqData);
+        })
+        .then(res => {
+            console.log(`\r\n单词 [${content}] 已成功添加到单词学习库。`);
         })
         .catch(error => {
             console.log(error);
         });
 }
-
-var loginReqOptions = shanbayApi.loginReqOptions;
-var loginInfo = {
-    username: config.username,
-    password: config.password
-};
-var loginPutData = JSON.stringify(loginInfo);
-loginReqOptions.headers['Content-Length'] = loginPutData.length;
-cbUtil.request(loginReqOptions, loginPutData)
-    .then(res => {
-        var cookiesObj = cbUtil.parseSetCookie(res.headers['set-cookie']);
-        fs.writeFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json'), JSON.stringify(cookiesObj));
-        var homepageReqOptions = shanbayApi.homepageReqOptions;
-        homepageReqOptions.headers.Cookie += cbUtil.getCookieStr(cookiesObj);
-        return new Promise((resolve, reject) => {
-            var homepageReq = http.request(homepageReqOptions, res => {
-                res.on('data', data => {
-
-                });
-                res.on('end', () => {
-                    var cookiesObj = JSON.parse(fs.readFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json')));
-                    var cookiesObj2 = cbUtil.parseSetCookie(res.headers['set-cookie']);
-                    for (key in cookiesObj2) {
-                        cookiesObj[key] = cookiesObj2[key];
-                    }
-                    fs.writeFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json'), JSON.stringify(cookiesObj));
-                    resolve(cookiesObj);
-                });
-            });
-            homepageReq.on('error', e => {
-                console.log(`problem with request: ${e.message}`);
-                reject(e);
-            });
-            homepageReq.end();
-        });
-    })
-    .then(cookiesObj => {
-        var searchReqOptions = shanbayApi.searchReqOptions;
-        searchReqOptions.path += content;
-        return cbUtil.request(searchReqOptions);
-    })
-    .then(res => {
-        var data = res.body.data;
-        var cnDefinitions = data.definitions.cn;
-        console.log(`单词 [${content}] 含义如下：\r\n`);
-        for (var i = 0; i < cnDefinitions.length; i++) {
-            console.log(`[${i + 1}] ${cnDefinitions[i].pos} ${cnDefinitions[i].defn}`);
-        }
-
-        var learnReqOptions = shanbayApi.learnReqOptions;
-        var learnReqData = JSON.stringify({
-            id: data.id,
-            content_type: 'vocabulary'
-        });
-        learnReqOptions.headers['Content-Length'] = learnReqData.length;
-        var cookiesObj = JSON.parse(fs.readFileSync(path.join(path.parse(__dirname).dir, 'storage/cookie.json')));
-        learnReqOptions.headers.Cookie += cbUtil.getCookieStr(cookiesObj);
-        return cbUtil.request(learnReqOptions, learnReqData);
-    })
-    .then(res => {
-        console.log(`\r\n单词 [${content}] 已成功添加到单词学习库。`);
-    })
-    .catch(error => {
-        console.log(error);
-    });
